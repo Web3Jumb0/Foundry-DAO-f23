0dd26be8dd83dc47716d13633078cbe8bd085942
Web3Jumb0 <ultimateli0430@gmail.com>
2024-07-23 00:29:02 +0800

    20240723-home-wee_hours

--- test/MyGovernorTest.t.sol
+++ test/MyGovernorTest.t.sol
@@ -8,10 +8,10 @@ import {TimeLock} from "../src/TimeLock.sol";
 import {GovToken} from "../src/GovToken.sol";
 
 contract MyGovernorTest is Test {
-    MyGovernor governor;
-    Box box;
-    TimeLock timeLock;
-    GovToken govToken;
+    MyGovernor private _governor;
+    Box private _box;
+    TimeLock private _timeLock;
+    GovToken private _govToken;
 
     address public USER = makeAddr("user");
     uint256 public constant INITIAL_SUPPLY = 100 ether;
@@ -22,29 +22,29 @@ contract MyGovernorTest is Test {
     uint256 public constant MIN_DELAY = 3600; // 1 hour
 
     function setUp() public {
-        govToken = new GovToken();
-        govToken.mint(USER, INITIAL_SUPPLY);
+        _govToken = new GovToken();
+        _govToken.mint(USER, INITIAL_SUPPLY);
 
         vm.startPrank(USER);
-        govToken.delegate(USER);
-        timelock = new TimeLock(MIN_DELAY, proposers, executors);
-        governor = new MyGovernor(govToken, timelock);
+        _govToken.delegate(USER);
+        _timeLock = new TimeLock(MIN_DELAY, proposers, executors);
+        _governor = new MyGovernor(_govToken, _timeLock);
 
-        bytes32 proposerRole = timelock.PROPOSER_ROLE();
-        bytes32 executorRole = timelock.EXECUTOR_ROLE();
-        bytes32 adminRole = timelock.TIMELOCK_ADMIN_ROLE();
+        bytes32 proposerRole = _timeLock.PROPOSER_ROLE();
+        bytes32 executorRole = _timeLock.EXECUTOR_ROLE();
+        bytes32 adminRole = _timeLock.TIMELOCK_ADMIN_ROLE();
 
-        timelock.grantRole(proposerRole, address(governor));
-        timelock.grantRole(executorRole, address(0));
-        timelock.revokeRole(adminRole, USER);
+        _timeLock.grantRole(proposerRole, address(_governor));
+        _timeLock.grantRole(executorRole, address(0));
+        _timeLock.revokeRole(adminRole, USER);
         vm.stopPrank();
 
-        box = new Box();
-        box.transferOwnership(address(timelock));
+        _box = new Box();
+        _box.transferOwnership(address(_timeLock));
     }
 
     function testCantUpdateBoxWithoutGovernance() public {
         vm.expectRevert();
-        box.store(1);
+        _box.store(1);
     }
 }
